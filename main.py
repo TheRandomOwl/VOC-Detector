777fef74b4e13764b90cc4e7db974e08f5bf3f23
Nathan Perry <46622921+TheRandomOwl@users.noreply.github.com>
2024-08-01 00:17:14 -0700

    save test fft program

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from voc import *
 
-file = signal('1kHz.txt', flip=False, baseline_shift=0)
+file = signal('20240730-0001 prop ctrl_0058.txt', flip=False, baseline_shift=0)
 file.smooth()
 file.plot('test')
-file.fft()
+file.fft2()
 file.plot_fft()
 
 # Plot the signal - file.x and file.y are the x and y values of the signal
