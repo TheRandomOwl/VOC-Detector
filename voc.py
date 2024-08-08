1261bf5c457e26ebd2cb7bd8a6641900b87acc39
Nathan Perry <46622921+TheRandomOwl@users.noreply.github.com>
2024-08-08 14:46:37 -0700

    Fix bug of cache not loading when cache is smoothness is zero and new instance is not zero

--- voc.py
+++ voc.py
@@ -5,7 +5,7 @@ For: LLU Volatile Organic Compound Detector Siganl Analysis
 Version: 10:50 am 6/23/2023
 
 Modified by: Nathan Perry and Nathan Fisher
-Version: 3.0.1
+Version: 3.0.2
 '''
 
 
@@ -350,14 +350,14 @@ class Run():
 		"""
 		
 		# True if signals are smoothed
-		self.smoothed = smoothness == 'default' or smoothness > 0 or smoothness == None
+		self.smoothed = smoothness == 'default' or smoothness > 0
 		self.smoothness = smoothness
 		
 		self.name = os.path.split(foldername)[1]
 
 		try:
 			run_cache = load()
-			if cache and self.name == run_cache.name and not (not self.smoothed and run_cache.smoothed) and run_cache.smoothness == smoothness:
+			if cache and self.name == run_cache.name and not (not self.smoothed and run_cache.smoothed) and (run_cache.smoothness == smoothness or run_cache.smoothness == 0):
 				self.signals = run_cache.signals
 				self.units = run_cache.units
 				if self.smoothed and not run_cache.smoothed:
