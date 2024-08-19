05ebd28025d7129d08cd08513a47d81b3863602d
Nathan Perry <46622921+TheRandomOwl@users.noreply.github.com>
2024-08-19 10:38:35 -0700

    Add --min option to cli tool

--- voc-cli.py
+++ voc-cli.py
@@ -8,7 +8,7 @@ A CLI tool to analyze data from Picoscope 7.
 Uses the voc module to analyze data from Picoscope 7 and plot signals.
 """
 
-VER = '0.1.0'
+VER = '0.2.0'
 API = voc.VER
 
 # Helper Functions
@@ -24,12 +24,13 @@ def validate_dir(path: Path):
 @click.option('--smoothness', type=click.IntRange(min=0), default=10, help="Smoothness of the data. Default is 10.")
 @click.option('--fft/--no-fft', default=False, help="Plot FFT instead of time-domain signal. Default is no-fft.")
 @click.option('--y-offset', type=float, default=0, help="Y-axis offset for the signal. Default is 0.")
+@click.option('--min', type=float, help="Minimum peak height for a signal to be included.")
 @click.pass_context
-def cli(ctx, cache, smoothness, fft, y_offset):
+def cli(ctx, cache, smoothness, fft, y_offset, min):
     """A CLI tool to analyze data from Picoscope 7."""
     # Store options in context
     ctx.ensure_object(dict)
-    ctx.obj.update(cache=cache, smoothness=smoothness, fft=fft, y_offset=y_offset)
+    ctx.obj.update(cache=cache, smoothness=smoothness, fft=fft, y_offset=y_offset, min=min)
 
 # CLI Commands
 @cli.command()
@@ -43,6 +44,9 @@ def plot(ctx, folder, save_dir):
     validate_dir(save_dir_path)
 
     signals = voc.Run(folder, cache=ctx.obj['cache'], smoothness=ctx.obj['smoothness'], y_offset=ctx.obj['y_offset'])
+    if ctx.obj['min'] != None:
+        signals.clean_empty(threshold=ctx.obj['min'])
+
     signals.plot(save_dir_path, fft=ctx.obj['fft'])
     click.echo(f"Plotted signals to folder: {save_dir_path}")
 
@@ -53,6 +57,9 @@ def average(ctx, folder):
     """Show the average signal or FFT for a run."""
     
     signals = voc.Run(folder, cache=ctx.obj['cache'], smoothness=ctx.obj['smoothness'], y_offset=ctx.obj['y_offset'])
+    if ctx.obj['min'] != None:
+        signals.clean_empty(threshold=ctx.obj['min'])
+    
     signals.show_avg_signal(fft=ctx.obj['fft'])
     click.echo(f"Displayed average signal for run in folder: {folder}")
 
@@ -69,9 +76,13 @@ def compare(ctx, folder_a, folder_b, save_dir):
     if save_path != None:
         validate_dir(save_path)
 
-    run_a = voc.Run(folder_a, cache=ctx.obj['cache'], smoothness=ctx.obj['smoothness'], y_offset=ctx.obj['y_offset'])
-    run_b = voc.Run(folder_b, cache=ctx.obj['cache'], smoothness=ctx.obj['smoothness'], y_offset=ctx.obj['y_offset'])
-    voc.plot_average_signals(run_a, run_b, save_path, fft=ctx.obj['fft'], show=show_plot)
+    A = voc.Run(folder_a, cache=ctx.obj['cache'], smoothness=ctx.obj['smoothness'], y_offset=ctx.obj['y_offset'])
+    B = voc.Run(folder_b, cache=ctx.obj['cache'], smoothness=ctx.obj['smoothness'], y_offset=ctx.obj['y_offset'])
+    if ctx.obj['min'] != None:
+        A.clean_empty(threshold=ctx.obj['min'])
+        B.clean_empty(threshold=ctx.obj['min'])
+    
+    voc.plot_average_signals(A, B, save_path, fft=ctx.obj['fft'], show=show_plot)
     
     if not show_plot:
         click.echo(f"Compared average signals and saved plot to {save_path}")
