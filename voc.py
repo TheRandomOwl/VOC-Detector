4dd7b26e237cbd220a1e6a37ceb1d17ebe145931
Nathan Perry <46622921+TheRandomOwl@users.noreply.github.com>
2024-08-27 15:10:02 -0700

    Bump version to 4.0.6

--- voc.py
+++ voc.py
@@ -18,7 +18,7 @@ import pickle  # A library for saving data in a python-readable format
 from scipy.integrate import trapezoid  # A library for numerical integration
 from tqdm import tqdm  # A library for progress bars
 
-VER = '4.0.5'
+VER = '4.0.6'
 
 METRIC = {
     '(us)': 1e-6,
