55acf2165f2477b154a301e20cd7727bbdcfdc8f
Nathan Perry <46622921+TheRandomOwl@users.noreply.github.com>
2024-09-01 21:30:02 -0700

    refactor: Update build script to use --noconfirm flag for pyinstaller commands

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ REM Install Python packages from requirements.txt
 pip install -r requirements.txt
 
 REM Create standalone executable for voc-gui.py
-pyinstaller --hide-console minimize-early voc-gui.py
+pyinstaller --noconfirm --hide-console minimize-early voc-gui.py
 
 REM Create standalone executable for voc-cli.py
-pyinstaller voc-cli.py
+pyinstaller --noconfirm voc-cli.py
 
 echo Finished creating executables.
 pause
